ccdce665866349da9419edfbcad17af0ad8c02d1
Ziqi Wang <ziqiw@mit.edu>
2020-07-04 23:34:05 -0400

    Adding templates

---
+++ templates/test.c
@@ -0,0 +1,15 @@
+
+
+void test_name() {
+  TEST_BEGIN();
+  // Test body goes here
+  TEST_PASS();
+  return;
+}
+
+int main() {
+  printf("========== main ==========\n");
+  // Tests go here
+  printf("==========================\n");
+  return 0;
+}
