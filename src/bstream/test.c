82e247501e93a28eebd9da40bcb063b6531dc9cb
Ziqi Wang <ziqiw@mit.edu>
2020-07-04 23:50:52 -0400

    Adding bstream

---
+++ src/bstream/test.c
@@ -0,0 +1,16 @@
+
+#include "stream.h"
+
+void test_name() {
+  TEST_BEGIN();
+  // Test body goes here
+  TEST_PASS();
+  return;
+}
+
+int main() {
+  printf("========== main ==========\n");
+  // Tests go here
+  printf("==========================\n");
+  return 0;
+}
