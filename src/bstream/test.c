64ca51b64a9dda5d91f3a40d11b909bbd57564b0
Ziqi Wang <ziqiw@mit.edu>
2020-07-06 21:09:25 -0400

    Finish test_advance

--- src/bstream/test.c
+++ src/bstream/test.c
@@ -19,9 +19,9 @@ void test_advance() {
   bstream_advance(bstream, 10);  // Noe crossing next boundary after byte advance
   assert(bstream_get_byte_pos(bstream) == 12 && bstream_get_bit_pos(bstream) == 4);
   bstream_advance(bstream, 13);  // Crossing next boundary after byte advance
-  assert(bstream_get_byte_pos(bstream) == 13 && bstream_get_bit_pos(bstream) == 1);
-  bstream_advance(bstream, 19 * 8 - 1);  // To the very end
-  assert(bstream_get_byte_pos(bstream) == 33 && bstream_get_bit_pos(bstream) == 0);
+  assert(bstream_get_byte_pos(bstream) == 14 && bstream_get_bit_pos(bstream) == 1);
+  bstream_advance(bstream, 18 * 8 - 1);  // To the very end
+  assert(bstream_get_byte_pos(bstream) == 32 && bstream_get_bit_pos(bstream) == 0);
   // This is the pos of the very end of the buffer
   assert(bstream_get_rem(bstream) == 0);
   assert(bstream_get_pos(bstream) == 32 * 8);
