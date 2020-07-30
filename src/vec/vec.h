0333ec2c81b27fa60c94017dc4f1ff25e22d29fd
Ziqi Wang <ziqiw@mit.edu>
2020-07-30 18:35:57 -0400

    Adding vec_t

--- src/vec/vec.h
+++ src/vec/vec.h
@@ -1,3 +1,8 @@
 
 #include "common.h"
 
+typedef struct {
+  int count;       // Current number of elements
+  int capacity;    // Maximum number of elements
+  void *data;      // Data array (could also store uint64_t in-place)
+} vec_t;
