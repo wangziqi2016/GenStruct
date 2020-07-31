c9ac8da8ca25257c593161e459db6742a01f4f84
Ziqi Wang <ziqiw@mit.edu>
2020-07-31 16:49:15 -0400

    Adding btree leaf

--- src/btree/btree.h
+++ src/btree/btree.h
@@ -12,6 +12,15 @@ typedef struct {
   void *value[BTREE_INNER_CAPACITY];
 } btree_inner_t;
 
+typedef struct {
+  int type;
+  int count;
+  void *key[BTREE_LEAF_CAPACITY];
+  void *value[BTREE_LEAF_CAPACITY];
+} btree_leaf_t;
 
+typedef struct {
+
+} btree_t;
 
 #endif
