124eef69347c47f3e9b91a9ab392f03607742e09
Ziqi Wang <ziqiw@mit.edu>
2020-07-30 21:29:32 -0400

    Adding iterator intf

--- src/vec/vec.h
+++ src/vec/vec.h
@@ -25,10 +25,26 @@ inline static void *vec_at(vec_t *vec, int index) { return *vec_addr(vec, index)
 
 void vec_push(vec_t *vec, void *value);
 void *vec_pop(vec_t *vec);
+// The following are just short hands
 inline static void vec_push_u64(vec_t *vec, uint64_t value) { vec_push(vec, (void *)value); }
-inline static uint64_t vec_pop_u64(vec_t *vec, uint64_t value) { return vec_pop(vec); }
+inline static uint64_t vec_pop_u64(vec_t *vec) { return (uint64_t)vec_pop(vec); }
+inline static void vec_push_32(vec_t *vec, int32_t value) { vec_push(vec, (void *)(uint64_t)value); }
+inline static int32_t vec_pop_32(vec_t *vec) { return (int32_t)vec_pop(vec); }
 
-inline static vec_count(vec_t *vec) { return vec->count; }
-inline static vec_capacity(vec_t *vec) { return vec->capacity; }
+// Get functions
+inline static int vec_get_count(vec_t *vec) { return vec->count; }
+inline static int vec_get_capacity(vec_t *vec) { return vec->capacity; }
+inline static void **vec_get_data(vec_t *vec) { return vec->data; }
+
+// Iterator
+typedef struct {
+  vec_t *vec;
+  int index;
+} vec_it_t;
+
+vec_it_t vec_it_begin(vec_t *vec) { return {vec, 0}; }
+int vec_it_isend(vec_it_t *it) { return it->index == it->vec->count; }
+void vec_it_next(vec_it_t *it) { assert(it->index >= 0 && it->index < it->vec->count); it->index++; }
+void *vec_it_value(vec_it_t *it) { return it->vec->data[it->index]; }
 
 #endif
