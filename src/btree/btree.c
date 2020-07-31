1ed5b215b1fbe6dd40ad91e3c16df64c34ea0c83
Ziqi Wang <ziqiw@mit.edu>
2020-07-31 17:04:28 -0400

    Adding btree_node_free

--- src/btree/btree.c
+++ src/btree/btree.c
@@ -11,7 +11,16 @@ btree_node_t *btree_node_init(int type) {
   return node;
 }
 
-
+// This function is recursive. It traverses all nodes below until leaf level is reached, and frees all of them
+void btree_node_free(btree_node_t *node) {
+  assert(node->type == BTREE_NODE_INNER || node->type == BTREE_NODE_LEAF);
+  if(node->type == BTREE_NODE_LEAF) return;
+  for(int i = 0;i < node->count;i++) {
+    btree_node_free(node->kv[i].value);
+  }
+  free(node);
+  return;
+}
 
 btree_t *btree_init() {
   btree_t *btree = (btree_t *)malloc(sizeof(btree_t));
