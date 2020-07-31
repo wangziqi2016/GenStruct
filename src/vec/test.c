e3a899ee4ca2fd800eb2466e6fde7040dd81f3a7
Ziqi Wang <ziqiw@mit.edu>
2020-07-31 14:53:48 -0400

    Adding test_addr

--- src/vec/test.c
+++ src/vec/test.c
@@ -38,8 +38,46 @@ void test_realloc() {
   return;
 }
 
+void test_push_pop() {
+  TEST_BEGIN();
+  vec_t *vec = vec_init_capacity(1);
+  const int count = 5000;
+  for(int i = 0;i < count;i++) {
+    vec_push_32(vec, i);
+  }
+  assert(vec_get_count(vec) == count);
+  assert(vec_get_capacity(vec) == nextlog2_32(count));
+  vec_free(vec);
+  TEST_PASS();
+  return;
+}
+
+void test_addr() {
+  TEST_BEGIN();
+  vec_t *vec = vec_init_capacity(1);
+  const int count = 5000;
+  for(int i = 0;i < count;i++) vec_push_32(vec, i);
+  for(int i = 0;i < count;i++) {
+    int32_t *p = vec_addr_32(vec, i);
+    assert(p[0] == i);
+    p[0] = i * 2;
+    p[1] = i * 2 + 1;
+  }
+  // Read as 64 bit
+  for(int i = 0;i < count;i++) {
+    uint64_t value = *vec_addr_u64(vec, i);
+    assert((int)(value & 0xFFFFFFFFUL) == i * 2);
+    assert((int)((value >> 32) & 0xFFFFFFFFUL) == i * 2 + 1);
+  }
+  vec_free(vec);
+  TEST_PASS();
+  return;
+}
+
 int main() {
   test_init();
   test_realloc();
+  test_push_pop(); // This also tests automatic realloc for push()
+  test_addr();
   return 0;
 }
