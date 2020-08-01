5011048483f1b6cf0cc7ba854df31450f8ad06eb
Ziqi Wang <ziqiw@mit.edu>
2020-07-31 22:54:42 -0400

    Adding call back types

--- src/btree/btree.h
+++ src/btree/btree.h
@@ -21,16 +21,24 @@ typedef struct {
   btree_kv_t kv[0];
 } btree_node_t;
 
+// Call back interface
+typedef int (*btree_key_cmp_func_t)(void *key1, void *key2);
+typedef int (*btree_node_search_func_t)(btree_node_t *node, void *key, int *index, btree_key_cmp_func_t key_cmp_func);
+
 typedef struct {
   btree_node_t *root;
+  btree_key_cmp_func_t key_cmp_func;    // Set to NULL if using uint64_t as key
+  btree_node_search_func_t search_func; // Set to either uint64_t search or customized key search
 } btree_t;
 
 btree_node_t *btree_node_init(int type);
 void btree_node_free(btree_node_t *node); // Recursively frees all nodes
-btree_t *btree_init();
+btree_t *btree_init();                    // By default use uint64_t as key type
+btree_t *btree_init_custom_key(btree_key_cmp_func_t key_cmp_func);
 void btree_free(btree_t *btree);
 
 // Returns the index found, or the next larger element
-int btree_node_search_u64(btree_node_t *node, uint64_t key, int *index);
+int btree_node_search_u64(btree_node_t *node, void *key, int *index, btree_key_cmp_func_t key_cmp_func);
+int btree_node_search(btree_node_t *node, void *key, int *index, btree_key_cmp_func_t key_cmp_func);
 
 #endif
