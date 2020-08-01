538e58b6a703e6448214ba887a44db01eccc9045
Ziqi Wang <ziqiw@mit.edu>
2020-08-01 19:49:30 -0400

    Finish btree_next_level

--- src/btree/btree.c
+++ src/btree/btree.c
@@ -144,12 +144,13 @@ btree_node_t *btree_next_level(btree_t *btree, btree_node_t *const node, void *k
   }
   // Then check B-Link property to see whether there is an unfinished node split
   assert(child->next == NULL || child->next->count > 0); // If not NULL then must be > 0
+  int b_link = 0;
   if(child->next != NULL) {
     void *child_sibling_key = child->next->kv[0].key;
-    int larger = btree->key_cmp_func ? \
+    b_link = btree->key_cmp_func ? \
       (btree->key_cmp_func(key, child_sibling_key) >= 0) : ((uint64_t)key >= (uint64_t)child_sibling_key);
     // There is indeed an unfinished node split, finish it by inserting into the current level
-    if(larger) {
+    if(b_link) {
       // If the current node is not large enough, then split it and that's all
       // The B-Link design ensures that the next thread seeing this will help along
       if(node->count < BTREE_INNER_CAPACITY) {
@@ -158,13 +159,13 @@ btree_node_t *btree_next_level(btree_t *btree, btree_node_t *const node, void *k
         btree_node_t *sibling = btree_node_split(node);
         void *sibling_key = sibling->kv[0].key;
         // Whether to insert into the sibling node or not
-        int right = btree->key_cmp_func ? \
+        int insert_right = btree->key_cmp_func ? \
           (btree->key_cmp_func(child_sibling_key, sibling_key) >= 0) : \
           ((uint64_t)child_sibling_key >= (uint64_t)sibling_key);
-        if(right) btree_node_insert(btree, sibling, child_sibling_key, child->next);
+        if(insert_right) btree_node_insert(btree, sibling, child_sibling_key, child->next);
         else btree_node_insert(btree, node, child_sibling_key, child->next);
       } 
     }
   }
-  return child;
+  return b_link ? child->next : child;
 }
