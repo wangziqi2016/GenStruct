d0e1c2e6a527e011f1a4c9e691d158a9ccd7ec0f
Ziqi Wang <ziqiw@mit.edu>
2020-08-08 15:46:24 -0400

    Fix test

--- src/bintree/test.c
+++ src/bintree/test.c
@@ -3,7 +3,9 @@
 
 // Generates random keys, inserts into the bintree and also put them into the array
 // Value is always key * 2 + 1
-static bintree_t *bintree_populate(int iter, uint64_t *array) {
+static bintree_t *bintree_populate(int iter, uint64_t **array) {
+  *array = (uint64_t *)malloc(sizeof(uint64_t) * iter);
+  SYSEXPECT(*array != NULL);
   rand_init();
   bintree_t *bintree = bintree_init();
   int array_index = 0;
@@ -11,17 +13,16 @@ static bintree_t *bintree_populate(int iter, uint64_t *array) {
     uint64_t key = rand_u64();
     int ret = bintree_insert(bintree, (void *)key, (void *)(key * 2 + 1));
     assert(ret == 1);
-    array[array_index++] = key;
+    (*array)[array_index++] = key;
   }
   return bintree;
 }
 
 void test_insert() {
   TEST_BEGIN();
-  int iter = 100000;
-  uint64_t *array = (uint64_t *)malloc(sizeof(uint64_t) * iter);
-  SYSEXPECT(arraay != NULL);
-  bintree_t *bintree = bintree_populate(iter, array);
+  int iter = 1000000;
+  uint64_t *array = NULL;
+  bintree_t *bintree = bintree_populate(iter, &array);
   for(int i = 0;i < iter;i++) {
     uint64_t key = array[i];
     void *value;
@@ -32,7 +33,11 @@ void test_insert() {
   return;
 }
 
+
+
 int main() {
   test_insert();
+  test_remove();
+  test_random_remove();
   return 0;
 }
