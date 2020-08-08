4d483a5f43500e38a345100fce1354508d1e0cae
Ziqi Wang <ziqiw@mit.edu>
2020-08-08 15:23:04 -0400

    Adding test_insert

--- src/bintree/test.c
+++ src/bintree/test.c
@@ -17,8 +17,19 @@ static bintree_t *bintree_populate(int iter, uint64_t *array) {
 }
 
 void test_insert() {
+  TEST_BEGIN();
   int iter = 100000;
-
+  uint64_t *array = (uint64_t *)malloc(sizeof(uint64_t) * iter);
+  SYSEXPECT(arraay != NULL);
+  bintree_t *bintree = bintree_populate(iter, array);
+  for(int i = 0;i < iter;i++) {
+    uint64_t key = array[i];
+    void *value;
+    int ret = bintree_search(bintree, (void *)key, &value);
+    assert(ret == 1 && (uint64_t)value == key * 2 + 1);
+  }
+  TEST_PASS();
+  return;
 }
 
 int main() {
