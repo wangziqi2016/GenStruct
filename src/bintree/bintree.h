9c66a0736ea34f8bcc83d3a5cafe4f8278053a4a
Ziqi Wang <ziqiw@mit.edu>
2020-08-08 14:25:13 -0400

    Adding key cmp and eq

--- src/bintree/bintree.h
+++ src/bintree/bintree.h
@@ -2,6 +2,16 @@
 #ifndef _BINTREE_H
 #define _BINTREE_H
 
+// The following must be overridden if the default key comparison function is not
+// built-in comparison operation
+#ifndef BINTREE_KEY_LESS
+#define BINTREE_KEY_LESS(x, y) ((uint64_t)x < (uint64_t)y)
+#else
+
+#ifndef BINTREE_KEY_EQ
+#define BINTREE_KEY_EQ(x, y) ((uint64_t)x == (uint64_t)y)
+#else 
+
 
 
 #endif
