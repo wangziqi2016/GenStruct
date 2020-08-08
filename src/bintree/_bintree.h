4750adb5bb9e2a9ea76f2b1677adc7856b64704c
Ziqi Wang <ziqiw@mit.edu>
2020-08-08 14:43:17 -0400

    Adding wrapper

---
+++ src/bintree/_bintree.h
@@ -0,0 +1,39 @@
+
+#ifndef _BINTREE_H
+#define _BINTREE_H
+
+#include "common.h"
+
+// The following must be overridden if the default key comparison function is not
+// built-in comparison operation
+// If you want to use customized comparison function, define the same macto in the wrapper file
+
+#ifndef BINTREE_KEY_LESS
+#define BINTREE_KEY_LESS(x, y) ((uint64_t)x < (uint64_t)y)
+#endif
+
+#ifndef BINTREE_KEY_EQ
+#define BINTREE_KEY_EQ(x, y) ((uint64_t)x == (uint64_t)y)
+#endif
+
+typedef struct bintree_node_struct_t {
+  void *key;
+  void *value;
+  struct bintree_node_struct_t *left;
+  struct bintree_node_struct_t *right;
+} bintree_node_t;
+
+typedef struct {
+  bintree_node_t *root;
+} bintree_t;
+
+bintree_node_t *bintree_node_init();
+void bintree_node_free(bintree_node_t *node); // This function recursively frees all child nodes below
+
+bintree_t *bintree_init();
+void bintree_free(bintree_t *bintree);
+
+// Returns 1 if insert succeeds; 0 if fails
+int bintree_insert(bintree_t *bintree, void *key, void *value);
+
+#endif
--- src/bintree/bintree.h
+++ src/bintree/bintree.h
@@ -1,34 +1,7 @@
 
-#ifndef _BINTREE_H
-#define _BINTREE_H
+// This file is just a wrapper for defining customized key comparison functions
 
-#include "common.h"
+// #define BINTREE_KEY_LESS(x, y) ... <- Returns 0 if not less, non-zero if less
+// #define BINTREE_KEY_EQ(x, y) ... <- Returns 0 if not eq, non-zero if eq
 
-// The following must be overridden if the default key comparison function is not
-// built-in comparison operation
-#ifndef BINTREE_KEY_LESS
-#define BINTREE_KEY_LESS(x, y) ((uint64_t)x < (uint64_t)y)
-#else
-
-#ifndef BINTREE_KEY_EQ
-#define BINTREE_KEY_EQ(x, y) ((uint64_t)x == (uint64_t)y)
-#else 
-
-typedef struct bintree_node_struct_t {
-  void *key;
-  void *value;
-  struct bintree_node_struct_t *left;
-  struct bintree_node_struct_t *right;
-} bintree_node_t;
-
-typedef struct {
-  bintree_node_t *root;
-} bintree_t;
-
-bintree_node_t *bintree_node_init();
-void bintree_node_free(bintree_node_t *node); // This function recursively frees all child nodes below
-
-bintree_t *bintree_init();
-void bintree_free(bintree_t *bintree);
-
-#endif
+#include "_bintree.h"
