56d85c71e020a4a79fac07a88f983bf615ebc29b
Ziqi Wang <ziqiw@mit.edu>
2020-10-02 19:53:59 -0400

    Fix typo

--- src/pq/pq.c
+++ src/pq/pq.c
@@ -3,7 +3,7 @@
 #include "pq.h"
 
 pq_t *pq_init_size(int size) {
-  pt_t *pq = (pq_t *)malloc(sizeof(pq_t));
+  pq_t *pq = (pq_t *)malloc(sizeof(pq_t));
   SYSEXPECT(pq != NULL);
   memset(pq, 0x00, sizeof(pq_t));
   pq->data = (void **)malloc(sizeof(void *) * size);
