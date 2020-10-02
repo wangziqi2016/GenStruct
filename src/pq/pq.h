531e41a47a5cdb83dbc9b99226800c7efaefc60f
Ziqi Wang <ziqiw@mit.edu>
2020-10-02 19:29:09 -0400

    Change decl

--- src/pq/pq.h
+++ src/pq/pq.h
@@ -9,13 +9,13 @@
 
 // Less than function for comparing elements in the queue; void * are general pointers pointing to the element
 // Returns 1 if a < b; 0 otherwise
-typedef int (*pq_less_cb_t)(void *a, void *b);
+// Users should define their own pq_less_cb either in the header or in the source
+int pq_less_cb(void *a, void *b);
 
 typedef struct {
   void **data;             // An array that might be reallocated
   int size;                // Current number of elements in the array
   int capacity;            // Maximim number of elements in the array
-  pq_less_cb_t less_cb;    // Element comparison function
 } pq_t;
 
 #endif
