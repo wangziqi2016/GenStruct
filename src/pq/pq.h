22fe56f9f9a2089f73df56ba8fec888eabd93776
Ziqi Wang <ziqiw@mit.edu>
2020-10-02 19:24:44 -0400

    Adding cbs

--- src/pq/pq.h
+++ src/pq/pq.h
@@ -4,4 +4,13 @@
 
 #include "common.h"
 
+// Initially allocate 128-element array
+#define PQ_INIT_SIZE       128
+
+// Less than function for comparing elements in the queue; void * are general pointers pointing to the element
+// Returns 1 if a < b; 0 otherwise
+typedef int (*pq_less_cb_t)(void *a, void *b);
+
+
+
 #endif
