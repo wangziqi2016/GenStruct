7abf18a1c88fb9788a30afde2fe90b380a65880e
Ziqi Wang <ziqiw@mit.edu>
2020-10-02 23:54:11 -0400

    Adding pq_test_node_t

--- src/pq/test.c
+++ src/pq/test.c
@@ -1,9 +1,24 @@
 
 #include "pq.h"
 
+typedef struct {
+  int key;
+  int value;
+} pq_test_node_t;
+
+
+
+void test_pq_push() {
+  TEST_BEGIN();
+
+  TEST_PASS();
+  return;
+}
+
+
 int main() {
   printf("========== pq ==========\n");
-  
+  test_pq_push();
   printf("========================\n");
   return 0;
 }
