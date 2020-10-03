fb37cbc62253bf956ce2c86cd953e6174a91a4a6
Ziqi Wang <ziqiw@mit.edu>
2020-10-03 00:19:31 -0400

    Adding pq iter functions

--- src/pq/test.c
+++ src/pq/test.c
@@ -7,7 +7,7 @@ typedef struct {
 } pq_test_node_t;
 
 // Note that the value is set to key * 2 + 1
-static node_t *pq_test_node_init(int key) {
+static pq_test_node_t *pq_test_node_init(int key) {
   pq_test_node_t *node = (pq_test_node_t *)malloc(sizeof(pq_test_node_t));
   SYSEXPECT(node != NULL);
   memset(node, 0x00, sizeof(pq_test_node_t));
@@ -23,7 +23,7 @@ static void pq_test_node_free(void *node) {
 }
 
 static int pq_test_node_less_cb(void *a, void *b) {
-  return a->key < b->key;
+  return ((pq_test_node_t *)a)->key < ((pq_test_node_t *)b)->key;
 }
 
 static pq_t *build_pq(int test_size, int init_size, int **array) {
@@ -48,6 +48,11 @@ void test_pq_push() {
   TEST_BEGIN();
   const int test_size = 1000;
   const int init_size = 3;
+  int *array = NULL;
+  // This inserts into the pq
+  pq_t *pq = build_pq(test_size, init_size, &array);
+
+  pq_free_all(pq, pq_test_node_free);
   TEST_PASS();
   return;
 }
