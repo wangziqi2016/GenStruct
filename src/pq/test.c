cdc5e9580564539ce05146f743bf4ec13b3d38e2
Ziqi Wang <ziqiw@mit.edu>
2020-10-02 23:54:21 -0400

    Adding pq_test_node_init()

--- src/pq/test.c
+++ src/pq/test.c
@@ -6,6 +6,15 @@ typedef struct {
   int value;
 } pq_test_node_t;
 
+// Note that the value is set to key * 2 + 1
+static node_t *pq_test_node_init(int key) {
+  pq_test_node_t *node = (pq_test_node_t *)malloc(sizeof(pq_test_node_t));
+  SYSEXPECT(node != NULL);
+  memset(node, 0x00, sizeof(pq_test_node_t));
+  node->key = key;
+  node->value = key * 2 + 1;
+  return node;
+}
 
 
 void test_pq_push() {
